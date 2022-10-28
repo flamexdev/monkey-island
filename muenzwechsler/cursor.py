669040ed40379e50f902a7c54b1daaf858dfde72
Flamex <contact@flamex.dev>
2022-10-28 14:02:18 +0200

    Update cursor.py

--- muenzwechsler/cursor.py
+++ muenzwechsler/cursor.py
@@ -1,5 +1,10 @@
 import pyautogui
+
+#Disable FailSafe
 pyautogui.FAILSAFE = False
 
+# Click on screen
 pyautogui.click(500, 500)
+
+# Move cursor at the bottom right corner to hide
 pyautogui.moveTo(pyautogui.size())
