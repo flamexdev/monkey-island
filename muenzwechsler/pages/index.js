e5e9482582287868ad4990c67b2959bcd2667295
Flamex <acc@flamex.dev>
2022-11-12 23:31:06 +0100

    improved displayed design

--- muenzwechsler/pages/index.js
+++ muenzwechsler/pages/index.js
@@ -28,58 +28,38 @@ export default function Home() {
     <div className={styles.container} onClick={() => {
       document.body.requestFullscreen()
     }}>
-      <h1 className={styles.heading}>
+      <h1 className={styles.title}>
         Münzwechsler
       </h1>
 
       <div className={styles.info}>
-        <p style={{
-          fontSize: "1.5rem",
-          fontWeight: "bold",
-          textDecoration: "underline",
-          margin: "20px",
-          padding: "0"
-        }}>Bitte PASSEND einwerfen!</p>
+        <p className={styles.info_heading}>Bitte PASSEND einwerfen!</p>
 
-        <table style={{
-          fontSize: "20px",
-          lineHeight: "1.6rem",
-          alignItems: "center",
-          width: "100%"
-        }}>
+        <table>
           {price_list?.map(e =>
             <tr>
-              <td>{e.amount} Monkey Taler = {e.price}€</td>
+              <td className={styles.table_cost}>{e.amount}</td>
+              <td className={styles.table_text}>Monkey Taler =</td>
+              <td className={styles.table_price}>{e.price}€</td>
             </tr>
           )}
         </table>
 
-        <p style={{
-          marginTop: "50px",
-          fontWeight: "bold",
-          backgroundColor: "#fff"
-        }}>
+        <p>
           Nur {price_list?.map(e => `${e.price}€ ${e.price < 5 ? "Münzen" : "Scheine"}`)
           .join(" / ")}
         </p>
 
-        <p style={{
-          marginTop: "20px",
-          fontWeight: "bold",
-          fontSize: "1.3rem",
-          textDecoration: "underline",
-          backgroundColor: "#39FF14",
-          width: "50%",
-          marginLeft: "25%"
-        }}>
-          !!! Kein Rückgeld !!!
+        <p className={styles.no_money_back}>
+          !!! Kein Rückgeld !!!<br/>
+          Keine Annahme von 2€ Münzen
         </p>
       </div>
 
       <Image
         src={"http://monkeyislandbochum.de/wp-content/uploads/2019/12/monkeyislandnew-b.png"}
-        width={120}
-        height={120}
+        width={180}
+        height={180}
         className={styles.watermark}
       />
     </div>
